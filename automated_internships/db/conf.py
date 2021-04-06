637cedf799a36706c5eb8060e6187bdb22e6a4a8
Aleksei Piskunov <piskunov.alesha@gmail.com>
2021-04-07 01:51:10 +0700

    Fix db config path

--- automated_internships/db/conf.py
+++ automated_internships/db/conf.py
@@ -5,7 +5,7 @@ TORTOISE_ORM = {
     "connections": {"default": os.environ["DATABASE_URL"]},
     "apps": {
         "models": {
-            "models": ["db.models", "aerich.models"],
+            "models": ["automated_internships.db.models", "aerich.models"],
             "default_connection": "default",
         },
     },
